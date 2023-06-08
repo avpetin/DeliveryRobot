e8c5c4707931c0a6c77cb5e28d5afd23e741b0b1
Alexey Petin <alex_beholder@mail.ru>
2023-06-08 16:29:59 +0300

    Update1

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static final Map<Integer, Integer> sizeToFreq = new LinkedHashMap<>();
+
+    public static void main(String[] args) throws InterruptedException {
+        final Thread[] threads = new Thread[1000];
+        for (int i = 0; i < threads.length; i++){
+            threads[i] = new Thread(()->{
+                String route = generateRoute("RLRFR", 100);
+                String[] rCountArray = route.split("R");
+                System.out.println("Kоличество команд поворота направо " + rCountArray.length);
+                synchronized (sizeToFreq){
+                    int value = sizeToFreq.get(rCountArray.length) == null ? 1 : sizeToFreq.get(rCountArray.length) + 1;
+                    sizeToFreq.put(rCountArray.length, value);
+                }
+            });
+            threads[i].start();
+            threads[i].join();
+        }
+
+        Map<Integer, Integer> sortedMap = sizeToFreq
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed())
+                .collect( Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2,
+                        LinkedHashMap::new));
+
+        int i = 0;
+        for(Map.Entry<Integer, Integer> entry : sortedMap.entrySet()){
+            if(i++ == 0) {
+                System.out.println("\nСамое частое количество повторений " + entry.getKey() +
+                        " (встретилось " + entry.getValue() + " раз)");
+                System.out.println("Другие размеры:");
+            }
+            else{
+                System.out.println("- " + entry.getKey() + " (" + entry.getValue() + " раз)");
+            }
+        }
+    }
+
+    public static String generateRoute(String letters, int length) {
+        Random random = new Random();
+        StringBuilder route = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            route.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return route.toString();
+    }
+}
